a3151556e534b4780dde7582a54848349a1d3c71
David Huggins-Daines <dhd@ecolingui.ca>
2023-05-29 21:53:51 -0400

    feat: update UI a bunch and make it more responsive

--- src/index.ts
+++ src/index.ts
@@ -17,14 +17,18 @@ interface Texte {
 
 class App {
   search_box: HTMLInputElement;
+  document_box: HTMLElement;
   search_results: HTMLElement;
+  media_query: MediaQueryList;
   index: Index;
   textes?: Array<Texte>;
 
   /* Get and construct objects */
   constructor() {
     this.search_box = document.getElementById("search-box") as HTMLInputElement;
+    this.document_box = document.getElementById("document-box")!;
     this.search_results = document.getElementById("search-results")!;
+    this.media_query = matchMedia("screen and (min-width: 48em)");
     this.index = new Index({
       tokenize: "forward",
       charset: "latin:advanced",
@@ -54,7 +58,7 @@ class App {
     if (this.textes === undefined)
       return;
     const text = this.search_box.value;
-    const results = this.index.search(text);
+    const results = this.index.search(text, 10);
     console.log(`${text}: ${JSON.stringify(results)}`);
     this.search_results.innerHTML = "";
     for (const idx of results) {
@@ -62,19 +66,23 @@ class App {
       const dt = document.createElement("dt");
       const a = document.createElement("a");
       const dd = document.createElement("dd");
+      a.href = `/${texte.document}#page=${texte.page + 1}`;
       if (texte.titre !== undefined) {
         a.innerText = texte.titre;
-        dd.innerText = texte.contenu.substring(0, 80);
+        dd.innerText = texte.contenu.substring(0, 80) + "...";
       }
       else {
         a.innerText = texte.contenu.substring(0, 30);
-        dd.innerText = texte.contenu.substring(0, 80);
+        dd.innerText = texte.contenu.substring(0, 80) + "...";
       }
-      a.addEventListener("click", () => {
-        PDFObject.embed(`/${texte.document}`, "#document-view", {
-          pdfOpenParams: { page: texte.page + 1, zoom: 100 },
-        });
-        return false;
+      a.addEventListener("click", (e) => {
+        /* Show the PDF in the page on large enough screens */
+        if (this.media_query.matches) {
+          PDFObject.embed(`/${texte.document}`, "#document-view", {
+            pdfOpenParams: { page: texte.page + 1, zoom: 100 },
+          });
+          e.preventDefault();
+        }
       });
       dt.append(a);
       this.search_results.append(dt);
@@ -86,9 +94,6 @@ class App {
   async initialize() {
     await this.read_index("/index");
     this.search_box.addEventListener("input", debounce(async () => this.search(), 200));
-    PDFObject.embed("/Rgl-1314-2021-L-Lotissement.pdf", "#document-view", {
-      pdfOpenParams: { page: 6 },
-    });
   }
 }
 
