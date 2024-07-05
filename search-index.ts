e99399e422a3ed8cedf45b3a9c1fd055ab63d317
David Huggins-Daines <dhd@ecolingui.ca>
2024-07-05 15:41:03 -0400

    fix: load index from alexi space

--- search-index.ts
+++ search-index.ts
@@ -1,12 +1,23 @@
 import lunr from "lunr";
 import fs from "node:fs";
-import folding from "lunr-folding";
+import stemmerSupport from "lunr-languages/lunr.stemmer.support";
+import lunrFR from "lunr-languages/lunr.fr";
+import unidecode from "unidecode";
 
-/* UGH! This API is SO WEIRD!!! */
-folding(lunr);
+stemmerSupport(lunr);
+lunrFR(lunr);
 
+lunr.Pipeline.registerFunction(token => token.update(unidecode), "unifold")
 const index = lunr.Index.load(JSON.parse(fs.readFileSync("public/index.json", "utf8")));
 
-for (const result of index.search("mobilité")) {
-  console.log(JSON.stringify(result));
+let i = 0;
+for (const result of index.search("règlement de démolition")) {
+    if (i++ == 5) break;
+    console.log(JSON.stringify(result));
+}
+console.log();
+i = 0;
+for (const result of index.search("t4.2")) {
+    if (i++ == 5) break;
+    console.log(JSON.stringify(result));
 }
--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,4 @@
 export const ALEXI_URL = "https://dhdaines.github.io/alexi";
+// Unfortunately this doesn't really work for testing because URL()
+// won't accept a relative URL as base (see https://github.com/whatwg/url/issues/531)
+// export const ALEXI_URL = "/alexi";
